f77a0bcc2787dca850b5fe285250dcf43ebe20b3
Bhagya <bhagyavishwajithwijesekara@gmail.com>
2025-03-20 13:36:56 +0530

    Create folder Structure

---
+++ project/frontend/src/farmer/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../elements/NavBar'
+import Card from '../elements/Card'
+
+function home() {
+  return (
+    <div>
+        <NavBar/>
+        <Card/>
+    </div>
+  )
+}
+
+export default home
